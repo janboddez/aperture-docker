3918f4f9f90f739ba4234f055adf9ec081f611fd
Jan Boddez <jan.boddez@gmail.com>
2020-01-14 22:38:03 +0100

    Production defaults

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -14,7 +14,10 @@ if [ ! -e /var/www/html/aperture/.env ]; then
 
     chown -R www-data:www-data .
 
+    sed -i "s|APP_ENV=local|APP_ENV=production|g" .env
+    sed -i "s|APP_DEBUG=true|APP_DEBUG=false|g" .env
     sed -i "s|APP_URL=http://localhost|APP_URL=${WATCHTOWER_CB:=https\://aperture.example.org}|g" .env
+
     sed -i "s|WATCHTOWER_URL=https://watchtower.dev|WATCHTOWER_URL=${WATCHTOWER_URL:=https\://watchtower.example.org}|g" .env
     sed -i "s|WATCHTOWER_CB=https://aperture.dev|WATCHTOWER_CB=${WATCHTOWER_CB}|g" .env
     sed -i "s|WATCHTOWER_TOKEN=1234|WATCHTOWER_TOKEN=${WATCHTOWER_TOKEN:=1234}|g" .env
@@ -27,6 +30,9 @@ if [ ! -e /var/www/html/aperture/.env ]; then
 
     sed -i "s|REDIS_HOST=127.0.0.1|REDIS_HOST=${REDIS_HOST:=127.0.0.1}|g" .env
     sed -i "s|REDIS_PASSWORD=null|REDIS_PASSWORD=${REDIS_PASSWORD:=null}|g" .env
+
+    sed -i "s|SQL_LOG_QUERIES=true|SQL_LOG_QUERIES=false|g" .env
+    sed -i "s|SQL_LOG_SLOW_QUERIES=true|SQL_LOG_SLOW_QUERIES=false|g" .env
 fi
 
 exec "$@"
