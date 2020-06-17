8b6b78253b1e5c18c7162b68845b17b0c89a1891
Jan Boddez <jan.boddez@gmail.com>
2020-06-17 12:16:24 +0200

    Remove Camo

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -22,9 +22,8 @@ if [ ! -e /var/www/html/aperture/.env ]; then
     sed -i "s|WATCHTOWER_CB=https://aperture.dev|WATCHTOWER_CB=${WATCHTOWER_CB}|g" .env
     sed -i "s|WATCHTOWER_TOKEN=1234|WATCHTOWER_TOKEN=${WATCHTOWER_TOKEN:=1234}|g" .env
 
-    sed -i "s|IMG_PROXY_URL=https://aperture-proxy.p3k.io/|IMG_PROXY_URL=${IMG_PROXY_URL:=https\://camo.example.org/}|g" .env
-    sed -i "s|IMG_PROXY_KEY=1234567890|IMG_PROXY_KEY=${IMG_PROXY_KEY:=1234567890}|g" .env
-
+    sed -i "s|IMG_PROXY_URL=https://aperture-proxy.p3k.io/|IMG_PROXY_URL=|g" .env
+    
     sed -i "s|DB_HOST=127.0.0.1|DB_HOST=${MYSQL_HOST:=127.0.0.1}|g" .env
     sed -i "s|DB_PASSWORD=secret|DB_PASSWORD=${MYSQL_PASSWORD:=aperture}|g" .env
 
